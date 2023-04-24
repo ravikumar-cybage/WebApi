d5f71613eca7a3f2d238badf9ef609fe4577bcb0
Ravi Kumar <ravkumar@cybage.com>
2023-04-24 10:39:47 +0530

    Add project files.

---
+++ WebApi/Controllers/MachineController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MachineController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+
+        {
+            MachineDetails machineDetials = new MachineDetails();
+
+            machineDetials.machineName = Environment.MachineName;
+
+            machineDetials.machineIP = Request.HttpContext.Connection.RemoteIpAddress.ToString();
+
+            machineDetials.machineOS = Environment.OSVersion.VersionString;
+
+            machineDetials.timestamp = DateTime.Now.ToString();
+
+            return Ok(machineDetials);
+
+
+
+
+
+        }
+    }
+}
---
+++ WebApi/MachineDetails.cs
@@ -0,0 +1,13 @@
+﻿namespace WebApi
+{
+    public class MachineDetails
+    {
+        public string? machineName { get; set; }
+
+        public string? machineIP { get; set; }
+
+        public string? machineOS { get; set; }
+
+        public string? timestamp { get; set; }
+    }
+}
